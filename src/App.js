be5c4bdccccf653e67b9f25c002df492fa038fa5
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 06:40:04 -0500

    feat: Use Recipes Provider.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
 import CategoriesProvider from "./context/CategoriesContext";
+import RecipesProvider from "./context/RecipesContext";
 
 function App() {
     return (
         <CategoriesProvider>
-            <Header />
-            <div className="container mt-5">
-                <div className="row">
-                    <Form />
+            <RecipesProvider>
+                <Header />
+                <div className="container mt-5">
+                    <div className="row">
+                        <Form />
+                    </div>
                 </div>
-            </div>
+            </RecipesProvider>
         </CategoriesProvider>
     );
 }
