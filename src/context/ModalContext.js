92c80bb3cf40dc6c8fecd8594c50b9f6135e9b02
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 07:41:26 -0500

    feat: Create recipe info state, useEffect to get recipe info depending on the id with axios.

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 // Create the context
 export const ModalContext = createContext();
@@ -7,6 +8,21 @@ export const ModalContext = createContext();
 const ModalProvider = props => {
     // State for id recipe
     const [idRecipe, setIdRecipe] = useState(null);
+
+    // State for recipe information
+    const [recipeInfo, setRecipeInfo] = useState({});
+
+    // Get recipe info when idRecipe is a number
+    useEffect(() => {
+        if (idRecipe) {
+            const getRecipe = async () => {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+                const res = await axios(url);
+                setRecipeInfo(res.data.drinks[0]);
+            };
+            getRecipe();
+        }
+    }, [idRecipe]);
 
     return (
         <ModalContext.Provider value={{ idRecipe, setIdRecipe }}>
