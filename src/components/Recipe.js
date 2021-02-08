4765a56faac80ad3f300f4fe5456df57e05b0141
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 08:10:33 -0500

    feat: Add a modal component from Material UI.

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,46 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: "absolute",
+        width: 600,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const Recipe = ({ recipe }) => {
+    // Modal Config
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+    const classes = useStyles();
+
+    const handleModalClick = () => {
+        setOpen(!open);
+    };
+
     // Use the modal Context
     const { setIdRecipe } = useContext(ModalContext);
 
     // Click the button
     const handleButtonClick = () => {
         setIdRecipe(recipe.idDrink);
+        handleModalClick();
     };
 
     return (
@@ -26,6 +59,17 @@ const Recipe = ({ recipe }) => {
                     >
                         See Recipe
                     </button>
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            handleModalClick();
+                            setIdRecipe(null);
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h1>From Modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
