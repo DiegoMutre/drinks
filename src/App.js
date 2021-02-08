aa823df3a31756f4155045e4acec8cb749658f07
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 07:31:35 -0500

    feat: Use Modal Provider in App component.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import Form from "./components/Form";
 import Header from "./components/Header";
 import RecipeList from "./components/RecipeList";
 import CategoriesProvider from "./context/CategoriesContext";
+import ModalProvider from "./context/ModalContext";
 import RecipesProvider from "./context/RecipesContext";
 
 function App() {
     return (
         <CategoriesProvider>
             <RecipesProvider>
-                <Header />
-                <div className="container mt-5">
-                    <div className="row">
-                        <Form />
+                <ModalProvider>
+                    <Header />
+                    <div className="container mt-5">
+                        <div className="row">
+                            <Form />
+                        </div>
+                        <RecipeList />
                     </div>
-                    <RecipeList />
-                </div>
+                </ModalProvider>
             </RecipesProvider>
         </CategoriesProvider>
     );
