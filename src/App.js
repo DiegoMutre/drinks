350bdbd67a160680cb9e494aa09a77dd6799e14d
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 06:20:47 -0500

    feat: Use the Category Context Provider.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
+import CategoriesProvider from "./context/CategoriesContext";
 
 function App() {
     return (
-        <>
+        <CategoriesProvider>
             <Header />
             <div className="container mt-5">
                 <div className="row">
                     <Form />
                 </div>
             </div>
-        </>
+        </CategoriesProvider>
     );
 }
 
