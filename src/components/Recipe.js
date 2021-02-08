cbcba2dc3adf8272fe546921d1d5a714e4c43032
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 08:51:50 -0500

    feat: Add scroll to Modal.

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -21,6 +21,8 @@ const useStyles = makeStyles(theme => ({
         backgroundColor: theme.palette.background.paper,
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
+        overflow: "auto",
+        maxHeight: "100vh",
     },
 }));
 
