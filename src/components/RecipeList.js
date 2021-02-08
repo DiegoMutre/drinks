3662e6f1cd6f338f6f282717d91041531abe8312
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 07:14:39 -0500

    feat: Create and use Recipe component.

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -6,7 +6,13 @@ const RecipeList = () => {
     // Use Recipes Context
     const { recipes } = useContext(RecipesContext);
 
-    return <div className="row mt-5"></div>;
+    return (
+        <div className="row mt-5">
+            {recipes.map(recipe => (
+                <Recipe key={recipe.idDrink} recipe={recipe} />
+            ))}
+        </div>
+    );
 };
 
 export default RecipeList;
