57513a268e4db7d1b4fb34de8410d0e5592e7938
DiegoMutre <tysonmutre10@gmail.com>
2021-02-08 05:50:18 -0500

    feat: Create and use Form component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Header from "./componets/Header";
+import Form from "./components/Form";
+import Header from "./components/Header";
 
 function App() {
     return (
         <>
             <Header />
+            <div className="container mt-5">
+                <div className="row">
+                    <Form />
+                </div>
+            </div>
         </>
     );
 }
